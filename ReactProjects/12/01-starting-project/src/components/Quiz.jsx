5ff4ea54b16f7e7235594ad085f167aabc037543
Gogi4aaa <georgi-07@abv.bg>
2024-06-13 22:27:10 +0300

    add final summary

--- ReactProjects/12/01-starting-project/src/components/Quiz.jsx
+++ ReactProjects/12/01-starting-project/src/components/Quiz.jsx
@@ -1,8 +1,7 @@
 import { useState, useCallback, useRef } from "react";
-import quizCompleteImg from "../assets/quiz-complete.png"
 import QUESTIONS from '../questions.js';
 import Question from "./Question.jsx";
-
+import Summary from "./Summary.jsx";
 const Quiz = () => {
     const [userAnswers, setUserAnswers] = useState([]);
 
@@ -19,10 +18,7 @@ const Quiz = () => {
 
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
     if(quizIsComplete){
-        return <div id="summary">
-            <img src={quizCompleteImg} alt="Trophy icon"/>
-            <h2>Quiz Completed!</h2>
-        </div>
+        return <Summary userAnswers={userAnswers}/>
     }
 
     return(
